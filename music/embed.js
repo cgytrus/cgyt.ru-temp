b043e49efc9c938bc88f6e8426f4e5a5967455a6
ConfiG <cgytrus@cgyt.ru>
2023-11-07 17:06:04 +0300

    parse t in yt embeds

--- music/embed.js
+++ music/embed.js
@@ -7,9 +7,25 @@ function tryEmbedYouTube(url) {
     if (match === null)
         return null;
     const id = match[1];
+    let start = 0;
+    const simpleTimeMatch = url.match(/http(?:|s):\/\/(?:youtu\.be\/.*?|(?:|www\.|music\.)youtube\.com\/watch)\?(?:|.*&)t=([0-9]+)(?:$|&).*/);
+    if (simpleTimeMatch !== null)
+        start = simpleTimeMatch[1];
+    else {
+        const complexTimeMatch = url.match(/http(?:|s):\/\/(?:youtu\.be\/.*?|(?:|www\.|music\.)youtube\.com\/watch)\?(?:|.*&)t=(?:|([0-9]+)d)(?:|([0-9]+)h)(?:|([0-9]+)m)(?:|([0-9]+)s)(?:$|&).*/);
+        start = 0;
+        if (complexTimeMatch[1] !== undefined)
+            start += parseInt(complexTimeMatch[1]) * 60 * 60 * 24;
+        if (complexTimeMatch[2] !== undefined)
+            start += parseInt(complexTimeMatch[2]) * 60 * 60;
+        if (complexTimeMatch[3] !== undefined)
+            start += parseInt(complexTimeMatch[3]) * 60;
+        if (complexTimeMatch[4] !== undefined)
+            start += parseInt(complexTimeMatch[4]);
+    }
     const iframe = document.createElement('iframe');
     iframe.width = '300px';
-    iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&fs=0`;
+    iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&fs=0&start=${start}`;
     iframe.frameBorder = 'none';
     return iframe;
 }
